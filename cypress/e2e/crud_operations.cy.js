c5dd53c9f5cc71f462cb4cd7346c0b1e8f5fb95d
Yohannes Assefa <yoniassefayoni@gmail.com>
2023-05-30 21:42:20 +0300

    add comments and cleanup

--- cypress/e2e/crud_operations.cy.js
+++ cypress/e2e/crud_operations.cy.js
@@ -1,6 +1,7 @@
 const email = "yoniassefayoni@gmail.com"
 const password = "Yohannes123"
 
+//Object containing the input field names and the values to be input as well as the expected errors from each input in arrays
 const courseFormInputAndExpectedErrors = {
     course_title: {
         input: ["", "1234", "Software Quality Assurance and Testing"],
@@ -48,60 +49,64 @@ const courseFormInputAndExpectedErrors = {
         errors: ["Course cost: Only numeric values are allowed", "Success"],
     },
 }
-
+//extracting keys and values from the object to clean up the code
 let validCourseFormInput = {}
-
-let values = Object.values(courseFormInputAndExpectedErrors).map((item) => {
+const courseFormInputNames = Object.keys(courseFormInputAndExpectedErrors)
+const courseFormInputValues = Object.values(courseFormInputAndExpectedErrors)
+let values = courseFormInputValues.map((item) => {
     return item.input.at(-1)
 })
-let keys = Object.keys(courseFormInputAndExpectedErrors)
+let keys = courseFormInputNames
 keys.forEach((key, index) => {
     validCourseFormInput[key] = values[index]
 })
 
 describe("Add Records", () => {
     context("Input Validation and form submission", () => {
+        // Code to be run before the execution of each "it" block
         beforeEach(() => {
             cy.loginByForm(email, password)
             cy.visit("http://localhost:5000/pages/add")
             cy.get("h3").should("contain", "Add New Record")
         })
-
+        // code to be run before the execution of the next context/it block
         before(() => {
+            //Check /support/commands.js
+            //custom command added to reset database to base state when called
             cy.test_cleanup()
         })
 
+        //the forms are validated upon submission, to validate one input we must input all the valid inputs in the fields before it so that we can see the errors for it, this loop does that
         Object.entries(courseFormInputAndExpectedErrors).forEach(
             (item, index) => {
+                //each test item named based on what field is being tested
                 it(`Tests ${item[0]} Validation`, () => {
                     item[1].input.forEach((text, idx) => {
                         if (index > 0) {
+                            //loop through all the previous inputs from the one we are testing now and insert the valid inputs into those, which are the last elements of the arrays. (array.at(-1))
                             for (let i = 0; i < index; i++) {
                                 cy.get(
-                                    `[name="${
-                                        Object.keys(
-                                            courseFormInputAndExpectedErrors
-                                        )[i]
-                                    }"]`
-                                ).type(
-                                    Object.values(
-                                        courseFormInputAndExpectedErrors
-                                    )[i].input.at(-1),
-                                    { delay: 0 }
-                                )
+                                    `[name="${courseFormInputNames[i]}"]`
+                                ).type(courseFormInputValues[i].input.at(-1), {
+                                    delay: 0,
+                                })
+                                //delay = 0 removes the typing delay
                             }
                         }
+                        //The reason text is being checked for is because the first input value at the beginning of the test is an empty string and cypress doesnt allow typing empty strings, making this conditional necessary
                         if (text) {
                             cy.get(`[name="${item[0]}"]`).type(text)
                         }
                         cy.get("[type=submit]").click()
-
+                        //checking if the last input to be tested is reached so we can check for success message instead of error message
                         if (item[1].errors[idx] == "Success") {
                             cy.get(".success-msg").should(
                                 "contain",
                                 "Record successfully added!"
                             )
-                        } else
+                        }
+                        // checking for error message when its not the last input
+                        else
                             [
                                 cy
                                     .get(".err-msg")
@@ -112,17 +117,14 @@ describe("Add Records", () => {
             }
         )
         it("Checks existing record adding", () => {
-            let inputs = Object.values(courseFormInputAndExpectedErrors).map(
-                (item) => {
-                    return item.input.at(-1)
-                }
-            )
+            //selecting all the last indices of the input array in the object (which are the valid inputs) to type into the fields and submit
+            let inputs = courseFormInputValues.map((item) => {
+                return item.input.at(-1)
+            })
 
-            Object.keys(courseFormInputAndExpectedErrors).forEach(
-                (input, index) => {
-                    cy.get(`[name=${input}]`).type(inputs[index])
-                }
-            )
+            courseFormInputNames.forEach((input, index) => {
+                cy.get(`[name=${input}]`).type(inputs[index])
+            })
             cy.get("[type=submit]").click()
             cy.get(".err-msg").should(
                 "contain",
@@ -135,6 +137,7 @@ describe("Add Records", () => {
         beforeEach(() => {
             cy.loginByForm(email, password)
         })
+        //Sending a post request directly to the server's add route to add a recorod
         it("can bypass the UI and add a record", () => {
             cy.request({
                 method: "POST",
@@ -146,7 +149,9 @@ describe("Add Records", () => {
                 expect(response.status).to.eq(200)
             })
             cy.visit("http://localhost:5000/pages/display")
+            //looking through the table to find the newly inserted course code
             cy.get("td:first-child").should((rows) => {
+                //callback function to perform any actions on items selected by cy.get which 'yields' the queried objects, much like JS getElementByTagName which yields a dom node
                 let items = Object.values(rows)
                     .slice(0, rows.length)
                     .map((item) => item.textContent)
@@ -156,6 +161,15 @@ describe("Add Records", () => {
             })
         })
 
+        // let items = Object.values(rows)
+        //             .slice(0, rows.length)
+        //             .map((item) => item.textContent)
+        //         expect(items).to.contain(
+        //             courseFormInputAndExpectedErrors.course_code.input.at(-1)
+        //         )
+        //this block of code, first grabs only the values from the key:value pair of an object and places them in a single array. the slice method removes the last element(object size) which is unnecessary to our operation, since we are working with DOM objects, we are mapping the array to return only the textContent attribute. then we look through the array containing only text to match the course code
+
+        // Testing the custom cypress command added to /support/commands.js for adding courses easily
         it("Custom cypress command for adding record", () => {
             cy.addRecord(validCourseFormInput)
             cy.visit("http://localhost:5000/pages/display")
@@ -190,6 +204,7 @@ describe("Read Records", () => {
         })
     })
     context("Searching records", () => {
+        //checking empty input for search
         it("Handles empty search", () => {
             cy.get('input[type="submit"]').click()
             cy.get(".success-msg").should(
@@ -256,28 +271,34 @@ describe("Edit records", () => {
         cy.test_cleanup()
     })
     context("Edit form validations", () => {
+        //similar execution to the add record validation
         Object.entries(courseFormInputAndExpectedErrors).forEach(
             (item, index) => {
-                it(`Tests ${item[0]} Validation`, () => {
+                it.only(`Tests ${item[0]} Validation`, () => {
                     item[1].input.forEach((text, idx) => {
-                        cy.get("input, textarea").each((item, i) => {
-                            cy.wrap(item).clear({ delay: 0 })
-                        })
-                        if (index > 0) {
-                            for (let i = 0; i < index; i++) {
-                                cy.get(
-                                    `[name="${
-                                        Object.keys(
-                                            courseFormInputAndExpectedErrors
-                                        )[i]
-                                    }"]`
-                                ).type(
-                                    Object.values(
+                        if (index < courseFormInputNames.length - 1) {
+                            cy.get(
+                                `[name=${
+                                    Object.keys(
                                         courseFormInputAndExpectedErrors
-                                    )[i].input.at(-1),
-                                    { delay: 0 }
-                                )
-                            }
+                                    )[index]
+                                }]`
+                            ).clear({ delay: 0 })
+                            cy.get(
+                                `[name=${
+                                    Object.keys(
+                                        courseFormInputAndExpectedErrors
+                                    )[index + 1]
+                                }]`
+                            ).clear({ delay: 0 })
+                        } else {
+                            cy.get(
+                                `[name=${
+                                    Object.keys(
+                                        courseFormInputAndExpectedErrors
+                                    )[index]
+                                }]`
+                            ).clear({ delay: 0 })
                         }
                         if (text) {
                             cy.get(`[name="${item[0]}"]`).type(text)
@@ -336,7 +357,7 @@ describe("Add Images", () => {
             "Error: You must select an image. Only image files [JPG | JPEG | PNG] are allowed!"
         )
     })
-    it.only("can upload image", () => {
+    it("can upload image", () => {
         const fileName = "logo.png"
         cy.get('input[type="file"]').attachFile(fileName)
         cy.get("[type=submit]").click()
@@ -345,7 +366,7 @@ describe("Add Images", () => {
             "Image is uploaded. Go back to Home page & check it."
         )
     })
-    it.only("Verify if image is uploaded", () => {
+    it("Verify if image is uploaded", () => {
         cy.visit("http://localhost:5000/homepage")
         cy.get("img").each((img) => {
             const src = img.attr("src")
